c72651fab43ec1f21cb4c62c3384b9904dc75aaa
Brando D <brando2147@gmail.com>
2021-01-23 23:36:12 -0700

    Updated readme template

--- index.js
+++ index.js
@@ -76,24 +76,93 @@ inquirer.prompt([
 ]).then(response => {
 
 
+    const answers =
+        `
+    # Title
+    
+    ${response.projectTitle}
+    
+    
+    ## Description
+    
+    ${response.projectDescription}
+    
+    
+    
+    ## Table of Contents
+    
+    [Description](#Description)
+    [About](#About)
+    [Built-With](#Built-with)
+    [Getting-Started](#Getting-Started)
+    [Demo](#Demo)
+    [Prerequisites](#Prerequisistes)
+    [Installation](#Installation)
+    [Tests](#Tests)
+    [Questions](#Questions)
+    [Usage](#Usage)
+    [Contributing](#Contributing)
+    [License](#License)
+    
+    
+    
+    ## About 
+    
+    [![${response.imgName}][product-screenshot]](${response.urlScreenshot})
+    
+    
+    
+    ### Built with 
+    
+    * [Bootstrap](https://getbootstrap.com)
+    * [JQuery](https://jquery.com)
+    * [Laravel](https://laravel.com)
+    
+     
+     
+    ## Getting Started
+    
+    ${response.gettingStarted}
+    
+    
+    
+    ### Prerequisites
+    
+    
+    
+    ### Installation
+    
+    
+    
+    
+    ## Demo
+    
+    
+    
+      
+    ## Contributing
+    
+    
+    
+     
+    ## License
+    
+    
+    
+    
+    ## Contact
+    
+    ${response.yourName} - ${response.yourEmail}
+    
+    Project Link: [${response.gitRepo}](${response.gitRepo})
 
-    const answers = ``
+
+    `
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    fs.writeFile('README.md', answers, (err) => {
+    fs.writeFile('Sample.md', answers, (err) => {
         err ? console.log("oops") : console.log("Yay!")
     })
 
